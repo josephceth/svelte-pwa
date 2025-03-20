6e16734405cf42269ce1db575ab803c951c69bf4
Chustz, Joseph <Joseph.Chustz@brownandroot.com>
2025-03-20 12:03:54 -0400

    doc navigation and size check for phone

--- src/lib/client/components/navigation/Dock.svelte
+++ src/lib/client/components/navigation/Dock.svelte
@@ -1,5 +1,9 @@
-<div class="dock">
-	<button>
+<script lang="ts">
+	import { goto } from '$app/navigation';
+</script>
+
+<div class="dock dock-lg">
+	<button on:click={() => goto('/')}>
 		<svg class="size-[1.2em]" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
 			><g fill="currentColor" stroke-linejoin="miter" stroke-linecap="butt"
 				><polyline
